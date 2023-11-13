f8608b2053d4517cdc933ce8fb929f500ce44ce8
mac yx <fsdf@f.com>
2023-11-13 10:17:09 +0800

    1

--- xiaogpt/bot/ernie4_bot.py
+++ xiaogpt/bot/ernie4_bot.py
@@ -60,7 +60,7 @@ class Ernie4Bot(ChatHistoryMixin, BaseBot):
 
         response = requests.request("POST", url, headers=headers, data=payload, timeout=(10, 100))
 
-        # print(response.text)
+        print(response.text)
         res_json = response.json()
         result = res_json['result']
         # print(result)
